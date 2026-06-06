3bbceb43f0ba330145ec2d00236a1780248d2dac
spiderasasen <spiderasasen@gmail.com>
2026-06-06 12:10:02 -0400

    changed the font and now im going to deal with the colors
    FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK

--- shop/src/pages/Home.jsx
+++ shop/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import Navigation_System from "../component/Navigation_System.jsx"
+import "../styles/main.css"
 
 function Home(){
     return(
         <>
-            <Navigation_System/>
+            <div>
+                <Navigation_System/>
+            </div>
+
+            <main>
+                <div>
+                    <h1>Welcome to our Shop</h1>
+                    <p>Explore our wide range of products</p>
+                </div>
+            </main>
         </>
     )
 }
