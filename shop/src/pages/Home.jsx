c8024ebd01cccdc319e36c0a4a91c63e41fb86dc
spiderasasen <spiderasasen@gmail.com>
2026-06-07 11:27:10 -0400

    made the home page. making the draft done

--- shop/src/pages/Home.jsx
+++ shop/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navigation_System from "../component/Navigation_System.jsx"
 import "../styles/main.css"
+import "../styles/home_page.css"
 
 function Home(){
     return(
@@ -9,9 +10,21 @@ function Home(){
             </div>
 
             <main>
-                <div>
-                    <h1>Welcome to our Shop</h1>
-                    <p>Explore our wide range of products</p>
+                <div className="main-content">
+                    <h1>Welcome to my Shop</h1>
+                    <p>If you love Hello Kitty, then you will love our Hello Kitty products!</p>
+                </div>
+
+                <div className="filiter_card_container">
+                    <div className="filiter_card1">
+                        <label>School Supplies</label>
+                    </div>
+                    <div className="filiter_card1">
+                        <label>Makeup</label>
+                    </div>
+                    <div className="filiter_card1">
+                        <label>Jewelry</label>
+                    </div>
                 </div>
             </main>
         </>
